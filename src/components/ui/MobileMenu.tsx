80e207b4ff411e9565c1add5976af8070e9aaaf5
Cokumu Junior <cokumujnr@gmail.com>
2025-09-30 07:40:07 +0300

    navbar and footer done

---
+++ src/components/ui/MobileMenu.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { SquareMenu } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { Button } from "./button";
+
+const MobileMenu = () => {
+    return (
+        <>
+            <div className="flex items-center gap-2">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button className="h-6 w-6" variant="ghost" size="icon">
+                            <SquareMenu />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem asChild>
+                            <a href="#projects" className="cursor-pointer">Projects</a>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                            <a href="#about" className="cursor-pointer">About</a>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                            <a href="#contact" className="cursor-pointer">Contact</a>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                            <a href="/resume.pdf" download className="cursor-pointer">Download CV</a>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+
+        </>
+    );
+};
+
+export default MobileMenu;
